08bb0a69bcbd269bab78ad079a5e0fb5b369d201
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-23 16:24:19 +0530

    Program now calculates Employees daily wage

--- src/com/blz/EmpWage.java
+++ src/com/blz/EmpWage.java
@@ -3,11 +3,17 @@ package com.blz;
 public class EmpWage {
 
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        int WAGE_PER_HOUR = 20;
+        int empHrs;
+        int EMP_IS_PRESENT = 1;
+        int emp_wage;
         double check = Math.floor(Math.random() * 10) % 2;
-        if (check == IS_FULL_TIME)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
+        if (check == EMP_IS_PRESENT) {
+            empHrs = 8;
+        } else
+            empHrs = 0;
+        emp_wage = empHrs * WAGE_PER_HOUR;
+        System.out.println("Employee Wage:" + emp_wage);
     }
+
 }
