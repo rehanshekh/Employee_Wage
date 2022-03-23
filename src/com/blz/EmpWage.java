5a89a004f1669fe52f5402fe2273a7cda2f7b7c0
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-23 16:31:11 +0530

    Added functionality for calculating part time Employee Wage

--- src/com/blz/EmpWage.java
+++ src/com/blz/EmpWage.java
@@ -3,17 +3,22 @@ package com.blz;
 public class EmpWage {
 
     public static void main(String[] args) {
+        int IS_PRESENT = 1;
+        int IS_PART_TIME = 2;
         int WAGE_PER_HOUR = 20;
         int empHrs;
-        int EMP_IS_PRESENT = 1;
-        int emp_wage;
-        double check = Math.floor(Math.random() * 10) % 2;
-        if (check == EMP_IS_PRESENT) {
+        int empwage;
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+        if (empcheck == IS_PRESENT)
             empHrs = 8;
-        } else
+        else if (empcheck == IS_PART_TIME)
+            empHrs = 4;
+        else
             empHrs = 0;
-        emp_wage = empHrs * WAGE_PER_HOUR;
-        System.out.println("Employee Wage:" + emp_wage);
+        empwage = empHrs * WAGE_PER_HOUR;
+        System.out.println("Employee Wage:" + empwage);
+
     }
 
 }
+
