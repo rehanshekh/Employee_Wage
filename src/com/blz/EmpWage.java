145b0cbbc6ee14ff7c8d8220b0aea1d220db8765
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-23 17:07:25 +0530

    Added functionality of calculating wage for a month or for given Hours

--- src/com/blz/EmpWage.java
+++ src/com/blz/EmpWage.java
@@ -5,22 +5,26 @@ public class EmpWage {
     public static final int IS_PRESENT = 1;
     public static final int IS_PART_TIME = 2;
     public static final int WAGE_PER_HOUR = 20;
-    public static final int NUM_OF_DAYS = 2;
+    public static final int NUM_OF_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 150;
 
     public static void main(String[] args) {
         int totalWage = 0;
         int salary = 0;
-        for (int day = 0; day < NUM_OF_DAYS; day++) {
+        int totalEmpHrs = 0;
+        int totalDays = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalDays < NUM_OF_DAYS) {
+            totalDays++;
             int random = (int) Math.floor(Math.random() * 10) % 3;
             int empHrs = switch (random) {
                 case IS_PART_TIME -> 4;
                 case IS_PRESENT -> 8;
                 default -> 0;
             };
-            salary = (empHrs * WAGE_PER_HOUR);
-            totalWage = totalWage + salary;
-
+            totalEmpHrs = totalEmpHrs + empHrs;
+            System.out.println("Day#: " + totalDays + " Emp Hr: " + empHrs);
         }
-        System.out.println("Salary is:" + salary);
+        totalWage = totalEmpHrs * WAGE_PER_HOUR;
+        System.out.println("Total Emp Wage: " + totalWage);
     }
 }
