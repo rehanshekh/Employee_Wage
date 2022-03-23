2ea97ec0f227a88df4371e1fd218618f39a66428
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-23 16:53:43 +0530

    Added acility to Calculating Employee Wage for a month

--- src/com/blz/EmpWage.java
+++ src/com/blz/EmpWage.java
@@ -2,18 +2,25 @@ package com.blz;
 
 public class EmpWage {
 
-        public static final int IS_PRESENT=1;
-        public static final int IS_PART_TIME=2;
-        public static  final int WAGE_PER_HOUR = 20;
-        public static void main(String[] args) {
+    public static final int IS_PRESENT = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int NUM_OF_DAYS = 2;
+
+    public static void main(String[] args) {
+        int totalWage = 0;
+        int salary = 0;
+        for (int day = 0; day < NUM_OF_DAYS; day++) {
             int random = (int) Math.floor(Math.random() * 10) % 3;
             int empHrs = switch (random) {
                 case IS_PART_TIME -> 4;
                 case IS_PRESENT -> 8;
                 default -> 0;
             };
-            int salary = (empHrs * WAGE_PER_HOUR);
-            System.out.println("Salary is:" + salary);
-        }
-    }
+            salary = (empHrs * WAGE_PER_HOUR);
+            totalWage = totalWage + salary;
 
+        }
+        System.out.println("Salary is:" + salary);
+    }
+}
