8a8fccab43274ae11610edea9006032a66646f2d
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-23 16:35:11 +0530

    Calculating Employee Wage using Switch Condition  Logic

--- src/com/blz/EmpWage.java
+++ src/com/blz/EmpWage.java
@@ -2,23 +2,18 @@ package com.blz;
 
 public class EmpWage {
 
-    public static void main(String[] args) {
-        int IS_PRESENT = 1;
-        int IS_PART_TIME = 2;
-        int WAGE_PER_HOUR = 20;
-        int empHrs;
-        int empwage;
-        double empcheck = Math.floor(Math.random() * 10) % 3;
-        if (empcheck == IS_PRESENT)
-            empHrs = 8;
-        else if (empcheck == IS_PART_TIME)
-            empHrs = 4;
-        else
-            empHrs = 0;
-        empwage = empHrs * WAGE_PER_HOUR;
-        System.out.println("Employee Wage:" + empwage);
-
+        public static final int IS_PRESENT=1;
+        public static final int IS_PART_TIME=2;
+        public static  final int WAGE_PER_HOUR = 20;
+        public static void main(String[] args) {
+            int random = (int) Math.floor(Math.random() * 10) % 3;
+            int empHrs = switch (random) {
+                case IS_PART_TIME -> 4;
+                case IS_PRESENT -> 8;
+                default -> 0;
+            };
+            int salary = (empHrs * WAGE_PER_HOUR);
+            System.out.println("Salary is:" + salary);
+        }
     }
-
-}
 
