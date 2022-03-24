2dd9d09605d2d3a60f0170ac37af7fd7b325f595
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-24 12:44:50 +0530

    Added ability to compute wage and save wage by Company name

--- src/com/blz/EmpWage.java
+++ src/com/blz/EmpWage.java
@@ -4,13 +4,28 @@ public class EmpWage {
 
     public static final int IS_PRESENT = 1;
     public static final int IS_PART_TIME = 2;
+    private final String company;
+    private final int empRate;
+    private final int numOfDays;
+    private final int maxHrs;
+    public int totalWage;
 
-    public static void main(String[] args) {
-        computeEmpWage("RIL",10,10,50);
+    EmpWage(String company, int empRate, int numOfDays, int maxHrs) {
+        this.company = company;
+        this.empRate = empRate;
+        this.numOfDays = numOfDays;
+        this.maxHrs = maxHrs;
     }
 
-    public static int computeEmpWage(String name, int empRatePerHr, int numOfDays, int maxHrs) {
-        int totalWage;
+    public static void main(String[] args) {
+        EmpWage emp1 = new EmpWage("RIL", 10, 10, 50);
+        emp1.computeEmpWage();
+        EmpWage emp2 = new EmpWage("DCW", 15, 25, 160);
+        emp2.computeEmpWage();
+    }
+
+    public void computeEmpWage() {
+        totalWage = 0;
         int totalEmpHrs = 0;
         int totalDays = 0;
         while (totalEmpHrs <= maxHrs && totalDays < numOfDays) {
@@ -24,9 +39,8 @@ public class EmpWage {
             totalEmpHrs = totalEmpHrs + empHrs;
             System.out.println("Day#: " + totalDays + " Emp Hr: " + empHrs);
         }
-        totalWage = totalEmpHrs * empRatePerHr;
-        System.out.println("Total Emp Wage for Company: "+name+" is " + totalWage);
-        return totalWage;
+        totalWage = totalEmpHrs * empRate;
+        System.out.println("Total Emp Wage for Company: " + company + " is " + totalWage);
     }
 }
 
